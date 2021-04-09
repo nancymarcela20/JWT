9862d47c573d44d0a6972d04853a8e71c7051b5d
nancymarcela20 <nancymarcelaag@ufps.edu.co>
2021-04-08 21:41:50 -0500

    Generar Token

---
+++ src/main/java/es/softteck/jwtDemo/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package es.softteck.jwtDemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping("hello")
+	public String helloWorld(@RequestParam(value="name", defaultValue="World") String name) {
+		return "Hello "+name+"!!";
+	}
+	
+	
+}
